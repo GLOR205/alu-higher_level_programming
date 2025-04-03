35dbba8e6e835b43f5a8614fa0c73b110031ada6
GLOR205 <g.muhorakey@alustudent.com>
2025-04-02 19:06:25 -0700

    post

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email
-sends a POST request to the passed URL with the email as a parameter,
-and displays the body of the response"""
-from urllib import parse, request
+import urllib.parse
+import urllib.request
 import sys
 
-
 if __name__ == "__main__":
-    values = {'email': sys.argv[2]}
-    data = parse.urlencode(values)
-    data = data.encode('ascii')
-    req = request.Request(sys.argv[1], data)
-    with request.urlopen(req) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
+    # Get URL and email from command line arguments
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Create a dictionary to hold the email data
+    data = {'email': email}
+
+    # Encode the data in application/x-www-form-urlencoded format
+    data = urllib.parse.urlencode(data).encode('utf-8')
+
+    # Make the POST request with the email parameter
+    with urllib.request.urlopen(url, data) as response:
+        # Read and decode the response
+        body = response.read().decode('utf-8')
+
+    # Print the body of the response
+    print(body)
+
